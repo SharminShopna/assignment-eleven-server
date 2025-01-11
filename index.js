a8014d5ff7f29963fadfa5fc1d3fc9d52f74f702
shopna's Macbook <sharminshopna149@gmail.com>
2025-01-11 18:17:23 +0600

    code improve

--- index.js
+++ index.js
@@ -240,7 +240,7 @@ app.patch('/review/:tutorId', async(req,res)=>{
   res.send(result);
 })
 
-    // Send a ping to confirm a successful connection
+    // Send a ping to confirm a successful 
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
